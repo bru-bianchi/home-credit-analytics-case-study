c2338736ed304fcdf71d1cad4224dd27b4b86a15
bru-bianchi <bruna.bbo@hotmail.com>
2026-05-12 14:00:01 -0300

    Transformações previous_application

--- sql/silver/040_previous_application.sql
+++ sql/silver/040_previous_application.sql
@@ -1,10 +1,49 @@
-CREATE SCHEMA IF NOT EXISTS silver;
-
-CREATE OR REPLACE TABLE silver.previous_application AS
+-- CREATE OR REPLACE TABLE silver.previous_application AS
 SELECT
     *,
 
+   ----------------------------------------------
+   -- PADRONIZAÇÃO DE DADOS PARA LEGIBILIDADE --
+   ----------------------------------------------
+
+   -- substituição de nulos categóricos - casos nulos já tratados na bronze
+    CASE WHEN name_type_suite IS NULL THEN 'Unknown' ELSE name_type_suite END as name_type_suite_clean,
+
+
+    -----------
+    -- FLAGS --
+    -----------
+
+    -- Razão do empréstimo
+    (name_goods_category IS NULL) as flag_goods_category_known,
+
+    -- Situação empréstimo
+    (ABS(days_decision) <= 30) AS flag_recent_application,
+    (cnt_payment >= 24) AS flag_long_term_payment,
+
+    (name_contract_status = 'Approved') AS flag_approved_application,
+    (name_contract_status = 'Refused') AS flag_refused_application,
+    (name_contract_status = 'Canceled') AS flag_canceled_application,
+
+    -- Tipo de empréstimo
+    (name_contract_type = 'Cash loans') AS flag_cash_loan,
+    (name_contract_type = 'Revolving loans') flag_revolving_loan,
+    (name_contract_type = 'Consumer loans') AS flag_consumer_loan,
+
+    -- Tipo de cliente
+    (name_client_type = 'Repeater') AS flag_repeater,
+
+    -------------
+    -- CÁCULOS --
+    -------------
+
     amt_credit - amt_application AS credit_application_diff,
+
+    CASE
+        WHEN amt_goods_price > 0
+        THEN amt_application / amt_goods_price
+        ELSE NULL
+    END AS goods_vs_application_ratio,
 
     CASE
         WHEN amt_application > 0
@@ -18,39 +57,5 @@ SELECT
         ELSE NULL
     END AS annuity_credit_ratio,
 
-    CASE
-        WHEN ABS(days_decision) <= 30 THEN TRUE
-        ELSE FALSE
-    END AS flag_recent_application,
-
-    CASE
-        WHEN name_contract_status = 'Approved' THEN TRUE
-        ELSE FALSE
-    END AS flag_approved_application,
-
-    CASE
-        WHEN name_contract_status = 'Refused' THEN TRUE
-        ELSE FALSE
-    END AS flag_refused_application,
-
-    CASE
-        WHEN name_contract_status = 'Canceled' THEN TRUE
-        ELSE FALSE
-    END AS flag_canceled_application,
-
-    CASE
-        WHEN name_contract_type = 'Revolving loans' THEN TRUE
-        ELSE FALSE
-    END AS revolving_loan_flag,
-
-    CASE
-        WHEN name_contract_type = 'Consumer loans' THEN TRUE
-        ELSE FALSE
-    END AS flag_consumer_loan,
-
-    CASE
-        WHEN cnt_payment >= 24 THEN TRUE
-        ELSE FALSE
-    END AS flag_long_term_payment
-
-FROM bronze.previous_application;
+FROM bronze.previous_application
+;
